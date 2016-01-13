8add3a49a1171b0c0deec885536f81a8a6765d39
Denis Cheung <d.s.h.cheung@gmail.com>
2016-01-13 14:01:35 +0800

    solution

--- independent-practice.js
+++ independent-practice.js
@@ -9,30 +9,29 @@
  * Question 1
  * Create an array of image sources. Use image1.png, image2.png, and image3.png as the array values.
  */
-
-// Your code here
+var q1 = ['image1.png', 'image2.png', 'image3.png'];
+console.log("Question 1: " + q1);
 
 /**
  * Question 2
  * Using the array from Question 1, store the first element of the array in variable q2.
  */
-
-// Your code here
+var q2 = q1[0]
+console.log("Question 2: " + q2);
 
 /**
  * Question 3
  * Get the length of the first array (number of elements in the array) and store it in variable q3
  */
-
-// Your code here
+var q3 = q1.length
+console.log("Question 3: " + q3);
 
 /**
  * Question 4
  *  Using the array from Question 1, store the last element of the array in variable q4. Hint: How can we get the number of elements in the array?
  */
-
-
-// Your code here
+var q4 = q1[q1.length-1];
+console.log("Question 4: " + q4);
 
 
 // ____________________________________________________________________________
@@ -49,14 +48,20 @@
  * Create an array of numbers using 1,2,3, and 4 as values. Use forEach to increase each value by 1 and store the value back in the array.
 The end result should be an array of numbers with values 2,3,4,5
  */
-
-// Your code here
+q1 = [1,2,3,4];
+q1.forEach(function(element,index){
+	q1[index]=++element;
+});
+console.log("Question 1: " + q1);
 
 
 /**
  * Question 2
  * Using the array from Question 1, find the average of the numbers in the array (average=sum of all numbers/number of numbers). Store the average in q2.
  */
-
-
-// Your code here
+q2 = 0;
+q1.forEach(function(element,index){
+	q2+=element;
+});
+q2=q2/q1.length;
+console.log("Question 2: " + q2);
