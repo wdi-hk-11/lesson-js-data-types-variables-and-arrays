0aeb907d67ec9f3b112856ce6aa818ed6cb91cd0
Lentz <lentz03@live.com>
2016-05-26 14:13:31 +0800

    done

--- independent-practice.js
+++ independent-practice.js
@@ -10,29 +10,33 @@
  * Create an array of image sources. Use image1.png, image2.png, and image3.png as the array values.
  */
 
-// Your code here
+var q1 = ['image1.png', 'image2.png', 'image3.png'];
+console.log("Question 1:" + q1);
 
 /**
  * Question 2
  * Using the array from Question 1, store the first element of the array in variable q2.
  */
 
-// Your code here
+var q2 = q1[0]
+console.log("Question 2:" + q2);
 
 /**
  * Question 3
  * Get the length of the first array (number of elements in the array) and store it in variable q3
  */
 
-// Your code here
+var q3 = q1.length
+console.log("Question 3:" + q3);
 
 /**
  * Question 4
  *  Using the array from Question 1, store the last element of the array in variable q4. Hint: How can we get the number of elements in the array?
  */
 
+var q4 = q1[q1.length-1];
+console.log("Question 4:" + q4);
 
-// Your code here
 
 
 // ____________________________________________________________________________
